048568523048f498cb22d0b0e01f60bf257b72b1
jacksteel <jacksteel531@icloud.com>
2023-08-20 23:24:38 -0400

    Added Web GUI

---
+++ spam_detection_web.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request
+from flask import render_template 
+import spam_detection as sd
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<p>Welcome to the Spam Detection System</p>"
+
+@app.route("detect")
+def detect():
+    return render_template('input_case.html')
+
+@app.route("/detect/<message>")
+def detect_message(message):
+    return sd.detect(message)
